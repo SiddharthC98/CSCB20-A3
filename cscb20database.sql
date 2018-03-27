8614402a3936dc3a6800c35116cca770f4ed4eec
rad-souza <37275230+rad-souza@users.noreply.github.com>
2018-03-27 17:45:07 -0400

    Added the SQL script that makes the cscb20 database, will have tables for User information given upon login and a table for marks.

---
+++ cscb20database.sql
@@ -0,0 +1,10 @@
+create database cscb20;
+use cscb20;
+
+create table users(
+	firstName varchar(120) NOT NULL,
+    lastName varchar(120) NOT NULL,
+    utorid varchar(8) NOT NULL,
+    email varchar(120) NOT NULL
+    );
+    
