a2d60adbdeeeae9ad6353fd641f4f12ad454c3ad
Siddharth Chitale <chitale.sid@gmail.com>
2018-03-27 22:24:28 -0400

    Updated connection page and added main login page

--- database_connect.php
+++ database_connect.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $db_name);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+// If connection is successful
 echo "Connection successful";
-$sql = "";
+
 ?>
---
+++ mainpagelogin.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+?>
+
+<html>
+<head>Login Page</head>
+
+<body>
+  <div class="login-box">
+    <h2>Login</h2>
+    <form>
+      <label>Username: </label>
+      <input type="text" name="username" placeholder="Enter username">
+      <br>
+      <label>Password: </label>
+      <input type="text" name="password" placeholder="Enter password">
+      <br>
+      <input type="submit" name="submit" value="Log In">
+    </form>
+  </div>
+</body>
+
+</html>
