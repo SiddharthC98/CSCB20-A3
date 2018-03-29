283e301c801416731b1d4fc6d8caa64dc696ac45
Siddharth Chitale <chitale.sid@gmail.com>
2018-03-28 22:09:13 -0400

    Made successful connection to db

--- database_connect.php
+++ database_connect.php
@@ -4,4 +4,9 @@
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'cscb20');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   if($db->connect_error) {
+     die("Connection failed: " . $db->connect_error);
+   }
+   echo "Connected successfully";
 ?>
