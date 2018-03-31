5b3287af84420a1d76396233bbba3116caed3312
rad-souza <37275230+rad-souza@users.noreply.github.com>
2018-03-31 16:32:54 -0400

    Made tables in the SQL file for marks and anonFeedback.

--- cscb20database.sql
+++ cscb20database.sql
@@ -8,3 +8,25 @@ create table users(
     email varchar(120) NOT NULL
     );
     
+create table marks(
+	id varchar(8) NOT NULL,
+    quiz1 numeric,
+    quiz2 numeric,
+    quiz3 numeric,
+    assignment1 numeric,
+    assignment2 numeric,
+    assignment3 numeric,
+    midterm numeric,
+    finalExam numeric,
+    practical numeric,
+    attendance numeric, #final Tutorial attendance mark
+    total numeric
+    );
+    
+create table anonFeedback(
+	q1 varchar(500),
+    q2 varchar(500),
+    q3 varchar(500),
+    q4 varchar(500)
+    );
+
