642c1810ffee6dfecbda25fc3034db5a4d271b4b
Siddharth Chitale <chitale.sid@gmail.com>
2018-04-02 15:39:30 -0400

    Changed some of the connection to the database

--- database_connect.php
+++ database_connect.php
@@ -1,7 +1,13 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'cscb20w18_h3');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
+   define('DB_USERNAME', 'chitales');
+   define('DB_PASSWORD', 'chitales');
+   define('DB_DATABASE', 'cscb20w18_chitales');
+   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   if(!$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)) {
+     die('Connection Failed: '.mysqli_connect_error());
+   }
+   //mainly for debudding purposes
+   echo "Connection Successful";
 ?>
--- getinfo.php
+++ getinfo.php
@@ -4,26 +4,15 @@
 <body>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "cscb20w18_h3";
+include("database_connect.php");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-$sql = "SELECT , ,  FROM instructors";
+$sql = "SELECT firstName, lastName, utorid FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo ": " . $row[""]. " - Name: " . $row[""]. " " . $row[""]. "<br>";
+        echo "UTORID: " . $row["utorid"]. "Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
     }
 } else {
     echo "0 results";
