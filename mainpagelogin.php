c4d4935d3100678d386db8fae4e7004897500129
Siddharth Chitale <chitale.sid@gmail.com>
2018-04-02 17:34:42 -0400

    Made login page work

--- mainpagelogin.php
+++ mainpagelogin.php
@@ -8,7 +8,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and password = '$mypassword'";
+      $sql = "SELECT id FROM loginInfo WHERE username = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $count = mysqli_num_rows($result);
@@ -19,86 +19,85 @@
          $_SESSION['login_user'] = $myusername;
          header("Location: welcome.php");
       }else {
-         $error = "Your Login Name or Password is invalid";
+         $error = "Your Login Name or Password is invalid! Check username and/or password.";
       }
    }
 ?>
 
 <html>
-<head>
-<title>Login Page</title>
+  <head>
+    <title>Login Page</title>
+    <style>
+      body {
+        background: url(https://www.fjordtravel.no/wp-content/uploads/2017/12/2BCAF065-B3CE-4117-B538-4D972F14AF68-1-1024x682.jpg);
+        background-size: cover;
+        background-position: center;
+        font-family: sans-serif;
+        margin: 0;
+        padding: 0;
+        z-index: -1;
+      }
 
-<style>
-body {
-  background: url(https://www.fjordtravel.no/wp-content/uploads/2017/12/2BCAF065-B3CE-4117-B538-4D972F14AF68-1-1024x682.jpg);
-  background-size: cover;
-  background-position: center;
-  font-family: sans-serif;
-  margin: 0;
-  padding: 0;
-}
+      .login-box {
+        width: 350px;
+        height: 420px;
+        background: rgba(0,0,0,0.5);
+        color: #fff;
+        top: 50%;
+        left: 50%;
+        position: absolute;
+        transform: translate(-50%,-50%);
+        box-sizing: border-box;
+        padding: 40px 30px;
+      }
 
-.login-box {
-  width: 350px;
-  height: 420px;
-  background: rgba(0,0,0,0.5);
-  color: #fff;
-  top: 50%;
-  left: 50%;
-  position: absolute;
-  transform: translate(-50%,-50%);
-  box-sizing: border-box;
-  padding: 70px 30px;
-}
+      h2 {
+        text-align: center;
+      }
 
-h2 {
-  text-align: center;
-}
+      .login-box label {
+        margin: 0;
+        padding: 0;
+      }
 
-.login-box label {
-  margin: 0;
-  padding: 0;
-}
+      .login-box input {
+        width: 100%;
+      }
 
-.login-box input {
-  width: 100%;
-}
+      .login-box input[type="submit"] {
+        width: 50%;
+        border: none;
+        outline: none;
+        height: 40px;
+        background: #1c8adb;
+        color: #fff;
+        font-size: 18px;
+        border-radius: 20px;
+        margin-top: 20px;
+      }
 
-.login-box input[type="submit"] {
-  width: 50%;
-  border: none;
-  outline: none;
-  height: 40px;
-  background: #1c8adb;
-  color: #fff;
-  font-size: 18px;
-  border-radius: 20px;
-  margin-top: 20px;
-}
-
-.login-box input[type="submit"]:hover {
-  cursor: pointer;
-  background: #39dc79;
-  color: black;
-}
-
-</style>
-
-</head>
+      .login-box input[type="submit"]:hover {
+        cursor: pointer;
+        background: #39dc79;
+        color: black;
+      }
+    </style>
+  </head>
 
 <body>
   <div class="login-box">
     <h2>Login</h2>
-    <form>
-      <label>Username: </label> <br>
+    <form action="" method="post">
+      <label><strong>Username:</strong></label> <br>
       <input type="text" name="username">
       <br>
       <br>
-      <label>Password: </label> <br>
-      <input type="text" name="password">
+      <label><strong>Password:</strong></label> <br>
+      <input type="password" name="password">
       <br>
       <br>
-      <input type="submit" name="submit" value="Log In">
+      <input type="submit" value="Log In">
+      <div style = "font-size:13px; color:white; margin-top:20px"><?php echo $error; ?></div>
     </form>
   </div>
 </body>
