b636ab02c141c2ed7b49814cd72ed80c78e88a30
Siddharth Chitale <chitale.sid@gmail.com>
2018-04-03 22:34:14 -0400

    Made it connect to a local db

--- database_connect.php
+++ database_connect.php
@@ -1,8 +1,8 @@
 <?php
-   define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
-   define('DB_USERNAME', 'chitales');
-   define('DB_PASSWORD', 'chitales');
-   define('DB_DATABASE', 'cscb20w18_chitales');
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'SiddharthC');
+   define('DB_PASSWORD', 'NightHawk038');
+   define('DB_DATABASE', 'mydb');
    $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
    if(!$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)) {
